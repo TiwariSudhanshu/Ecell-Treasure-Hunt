17ae0f9aad74a1ecbb8d6b8b336e5c0a5fa6cd29
Vihan Patidar <1vihanpatidar6@gmail.com>
2024-10-19 17:13:38 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
+
 import { Route,  Routes } from "react-router-dom"
 import Leaderboard from './components/Leaderboard'
 import Invalidloc from './components/InvalidLocn'
-
-=======
-import { Route,  Routes } from "react-router-dom";
-import TeamEntry from "./teamEntry";;
+import TeamEntry from "./teamEntry";
 import TeamDashboard from "./teamDashboard";
->>>>>>> 053918f06639387001e7864b9ca0c0a9bbbf724e
+
 function App() {
 
   return (
     <>
       <Routes>
-<<<<<<< HEAD
-      <Route path='/' element={<h1>Hey</h1>}/>
       <Route path='/leaderboard' element={<Leaderboard/>}/>
       <Route path='/invalidlocation' element={<Invalidloc/>}/>
-=======
-    
       <Route path="/" element={<TeamEntry />} />
       <Route path="/team/:teamId" element={<TeamDashboard />} />
->>>>>>> 053918f06639387001e7864b9ca0c0a9bbbf724e
       </Routes>
     </>
   )
